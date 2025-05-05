762efd9c658b05f327e2dcec59a6c48811bdc77c
dilip0418 <sudheer0418@gmail.com>
2025-05-06 00:09:31 +0530

    Added Service Task

--- src/main/java/com/company/ModelConsumeService.java
+++ src/main/java/com/company/ModelConsumeService.java
@@ -1,4 +1,12 @@
 package com.company;
 
+import com.company.model.Model;
+
 public class ModelConsumeService {
+
+    public Model getModel(Model model) {
+        System.out.println("This is model object: " + model);
+        model.setMessage("Obtained from ModelConsumeService class");
+        return model;
+    }
 }
